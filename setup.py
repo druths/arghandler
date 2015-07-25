44e9274242a9a30fc1cf35864ef54ea827362652
Derek Ruths <druths@networkdynamics.org>
2015-07-25 09:40:25 -0700

    Added basic installation script

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(	name='arghandler',
+		version='1.0',
+		description='argparse extended with awesome feature enhancements to make life easier',
+		author='Derek Ruths',
+		author_email='druths@networkdynamics.org',
+		url='http://www.github.com/druths/arghandler',
+		packages=['arghandler','arghandler.tests']
+	)
