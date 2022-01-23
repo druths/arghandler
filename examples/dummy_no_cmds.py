52458d2c01f586711c8260feff82fcd4b1fb1bed
Derek Ruths <derek@derekruths.com>
2022-01-23 11:47:04 +0000

    Added an example for no subcommands

--- examples/dummy_no_cmds.py
+++ examples/dummy_no_cmds.py
@@ -3,14 +3,9 @@
 
 from arghandler import ArgumentHandler, subcmd
 
-@subcmd('echo')
-def echo(parser,context,args):
-    print args  
-
-@subcmd('add')
-def add(parser,context,args):
-    print sum(args)
-
 if __name__ == '__main__':
     handler = ArgumentHandler(enable_autocompletion=True)
-    handler.run()
+
+    args = handler.parse_args()
+
+    print('done')
